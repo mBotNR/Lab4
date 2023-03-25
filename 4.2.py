4e0ff2ab2b02d1a6b95c62079c807f230f72668b
k1_249_06 <rohin.nilta@gmail.com>
2023-03-25 13:51:44 +0300

    LAb4

--- 4.2.py
+++ 4.2.py
@@ -1,15 +1,25 @@
 class Stack:
-    def __init__(self):
-        self.stack =[]
-
-    def __push__(self, item):
-        self.stack.append(item)
+    def __init__(self, stack, max):
+        self.stack = [1, 2, 3]
+        self.max = [3, 4, 1]
 
     def __pop__(self):
         if len(self.stack) == 0:
             return None
         removed = self.stack.pop()
+        if len(self.stack) == 0:
+            self.max = None
+        elif removed == self.max:
+            self.max = self.stack[0]
+            for value in self.stack:
+                if value > self.max:
+                    self.max = value
         return removed
+
+    def __push__(self, item):
+        self.stack.append(item)
+        if len(self.stack) == 1 or item > self.max:
+            self.max = item
 
     def __append__(self):
         pass
@@ -21,3 +31,4 @@ class Stack:
         pass
 
     def __next__(self):
+        pass
